276f26a31bbd0b6c4bf866870ca8e9d72fc09cf7
The Lost Toastr <josht98@aol.com>
2024-04-03 14:20:41 -0400

    Init Solito

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,19 @@
 module.exports = {
-  extends: 'next',
+  extends: ['next', 'next/core-web-vitals', 'prettier'],
+  plugins: ['prettier', '@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json', // Adjust if your tsconfig is located elsewhere
+  },
   settings: {
     next: {
       rootDir: 'apps/next/',
     },
   },
+  rules: {
+    'prettier/prettier': 'error',
+    // TypeScript specific rules or overrides
+    '@typescript-eslint/explicit-function-return-type': 'off', // Example rule
+  },
   root: true,
-}
+};
